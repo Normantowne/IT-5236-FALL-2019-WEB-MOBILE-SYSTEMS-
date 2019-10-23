76906d8c5f51b7b5c1bc00e4dd9dc2cec0169e29
Normantowne <46829076+Normantowne@users.noreply.github.com>
2019-10-23 14:59:06 -0400

    Update tasks endpoint to actually work correctly

--- api/tasks.php
+++ api/tasks.php
@@ -7,17 +7,17 @@ error_reporting(E_ALL);
 $dbconnecterror = FALSE;
 $dbh = NULL;
 require_once 'credentials.php';
-	
+
 try{
-	
+
 	$conn_string = "mysql:host=".$dbserver.";dbname=".$db;
-	
+
 	$dbh= new PDO($conn_string, $dbusername, $dbpassword);
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	
+
 }catch(Exception $e){
 	//$dbconnecterror = TRUE;
-	
+
 	//This means we had some database issues
 	http_response_code(504);
 	exit();
@@ -25,67 +25,21 @@ try{
 
 
 //GET Task
-if ($_SERVER['REQUEST_METHOD'] == "GET") {  
-    if (array_key_exists('listID', $_GET)){
-		$listID = $_GET['listID']; 
-	}else {
-		//Could not be found
-		http_response_code(404);
-		echo "id error";
-		exit();
-	}
-	
-	//decoding the json body from the request
-    $task = json_decode(file_get_contents('php://input'), true);
-	
-    if (array_key_exists('completed', $task)) {
-		$complete = $task["completed"];
-	} else {
-		//Could not be found
-		http_response_code(404);
-		echo "complete error";
-		exit();
-	}
-
-	// add the other two fields here
-	if (array_key_exists('taskName', $task)) {
-		$taskName = $task["taskName"];
-	} else {
-		//Could not be found
-		http_response_code(404);
-		echo "taskName error";
-		exit();
-	}
-		
-	
-	if (array_key_exists('taskDate', $task)) {
-		$taskDate = $task["taskDate"];
-	} else {
-		//Could not be found
-		http_response_code(404);
-		echo "taskDate error";
-		exit();
-	}
-	
-if (!$dbconnecterror) {
+if ($_SERVER['REQUEST_METHOD'] == "GET") {
+	if (!$dbconnecterror) {
 		try {
-			$sql = "SELECT doList WHERE listID=:listID";
-			$stmt = $dbh->prepare($sql);			
-			$stmt->bindParam(":listID", $listID);
+			$sql = "SELECT * FROM doList";
+			$stmt = $dbh->prepare($sql);
 			$response = $stmt->execute();
-			http_response_code(204);
+			http_response_code(200);
+			echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC))
 			exit();
-			
-		} catch (PDOException $e) {
-		//This means we had some database issues
-		http_response_code(504);
-		exit();
-		
-		}	
-	} else {
-		//This means we had a bad gateway
-		http_response_code(502);
-		echo "database error";
-		exit();
-	}
 
+		} catch (PDOException $e) {
+			//This means we had some database issues
+			http_response_code(504);
+			echo 'could not get tasks';
+			exit();
+		}
+	}
+}
