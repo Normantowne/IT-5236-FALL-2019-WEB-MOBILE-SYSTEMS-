b532562d2731a5fe6d75c5ff8a1e2ce13cdb3c06
Normantowne <46829076+Normantowne@users.noreply.github.com>
2019-10-23 15:28:56 -0400

    Update api

--- api/task.php
+++ api/task.php
@@ -7,28 +7,28 @@ error_reporting(E_ALL);
 $dbconnecterror = FALSE;
 $dbh = NULL;
 require_once 'credentials.php';
-	
+
 try{
-	
+
 	$conn_string = "mysql:host=".$dbserver.";dbname=".$db;
-	
+
 	$dbh= new PDO($conn_string, $dbusername, $dbpassword);
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	
+
 }catch(Exception $e){
 	//$dbconnecterror = TRUE;
-	
+
 	//This means we had some database issues
 	http_response_code(504);
 	exit();
-} 
+}
 
 
 
 //Update Task
-if ($_SERVER['REQUEST_METHOD'] == "PUT") {  
+if ($_SERVER['REQUEST_METHOD'] == "PUT") {
     if (array_key_exists('listID', $_GET)){
-		$listID = $_GET['listID']; 
+		$listID = $_GET['listID'];
 	}else {
 		//Could not be found
 		http_response_code(404);
@@ -36,10 +36,10 @@ if ($_SERVER['REQUEST_METHOD'] == "PUT") {
 		exit();
 	}
 	var_dump
-	
+
 	//decoding the json body from the request
     $task = json_decode(file_get_contents('php://input'), true);
-	
+
     if (array_key_exists('completed', $task)) {
 		$complete = $task["completed"];
 	} else {
@@ -58,8 +58,8 @@ if ($_SERVER['REQUEST_METHOD'] == "PUT") {
 		echo "taskName error";
 		exit();
 	}
-		
-	
+
+
 	if (array_key_exists('taskDate', $task)) {
 		$taskDate = $task["taskDate"];
 	} else {
@@ -68,11 +68,11 @@ if ($_SERVER['REQUEST_METHOD'] == "PUT") {
 		echo "taskDate error";
 		exit();
 	}
-	
+
 if (!$dbconnecterror) {
 		try {
 			$sql = "UPDATE doList SET complete=:complete, listItem=:listItem, finishDate=:finishDate WHERE listID=:listID";
-			$stmt = $dbh->prepare($sql);			
+			$stmt = $dbh->prepare($sql);
 			$stmt->bindParam(":complete", $complete);
 			$stmt->bindParam(":listItem", $taskName);
 			$stmt->bindParam(":finishDate", $taskDate);
@@ -80,26 +80,26 @@ if (!$dbconnecterror) {
 			$response = $stmt->execute();
 			http_response_code(204);
 			exit();
-			
+
 		} catch (PDOException $e) {
 		//This means we had some database issues
 		http_response_code(504);
 		exit();
-		
-		}	
+
+		}
 	} else {
 		//This means we had a bad gateway
 		http_response_code(502);
 		echo "database error";
 		exit();
-    
+
 	}
 //CREATE
 } else if ($_SERVER['REQUEST_METHOD'] == "POST"){
 	//decoding the json body from the request
     $task = json_decode(file_get_contents('php://input'), true);
-	
-    if (array_key_exists('completed', $task)) {
+
+  if (array_key_exists('completed', $task)) {
 		$complete = $task["completed"];
 	} else {
 		//Could not be found
@@ -117,8 +117,8 @@ if (!$dbconnecterror) {
 		echo "taskName error";
 		exit();
 	}
-		
-	
+
+
 	if (array_key_exists('taskDate', $task)) {
 		$taskDate = $task["taskDate"];
 	} else {
@@ -127,71 +127,71 @@ if (!$dbconnecterror) {
 		echo "taskDate error";
 		exit();
 	}
-	
+
 	if (!$dbconnecterror) {
 		try {
 			$sql = "INSERT INTO doList (listItem, finishDate, complete) VALUES (:listItem, :finishDate, :complete)";
-			$stmt = $dbh->prepare($sql);			
+			$stmt = $dbh->prepare($sql);
 			$stmt->bindParam(":complete", $complete);
 			$stmt->bindParam(":listItem", $taskName);
 			$stmt->bindParam(":finishDate", $taskDate);
-			$stmt->execute();	
+			$stmt->execute();
 			$taskID = $dbh->lastInsertId();
 			http_response_code(201);
 			echo json_encode(["ID" => taskID]);
 			exit();
-			
-			
+
+
 		} catch (PDOException $e) {
 		//This means we had some database issues
 		http_response_code(504);
 		exit();
-		
-		}	
+
+		}
 	} else {
 		//This means we had a bad gateway
 		http_response_code(502);
 		echo "database error";
 		exit();
-    
+
 	}
-	
-//DELETE	
+
+//DELETE
 } else if ($_SERVER['REQUEST_METHOD'] == "DELETE"){
 	//find the ID of the task you want to DELETE
 	if (array_key_exists('listID', $_GET)){
-		$listID = $_GET['listID']; 
+		$listID = $_GET['listID'];
 	}else {
 		//Could not be found
 		http_response_code(404);
 		echo "id error";
 		exit();
-		
+
 	}
-	
+
 	if (!$dbconnecterror) {
 		try {
 			//deletes all from the table where the list ID is the given list ID
 			$sql = "DELETE FROM doList WHERE listID=:listID";
-			$stmt = $dbh->prepare($sql);			
+			$stmt = $dbh->prepare($sql);
 			$stmt->bindParam(":listID", $listID);
 			$response = $stmt->execute();
 			http_response_code(204);
 			exit();
-			
+
 		} catch (PDOException $e) {
 		//This means we had some database issues
 		http_response_code(504);
 		echo "check your internet connection";
 		exit();
-		
-		}	
+
+		}
 	} else {
 		//This means we had a bad gateway
 		http_response_code(502);
 		echo "database error";
 		exit();
-    
+
 	}
 } else {
 		//METHOD NOT ALLOWED
