61578004b96551730c3b2668c5ab521d53c36c78
Normantowne <46829076+Normantowne@users.noreply.github.com>
2019-11-04 13:38:53 -0500

    Remove unneeded $dbconnecterror checks

--- api/tasks.php
+++ api/tasks.php
@@ -1,6 +1,5 @@
 <?php
 // Declare the credentials to the database
-$dbconnecterror = FALSE;
 $dbh = NULL;
 require_once 'credentials.php';
 
@@ -12,8 +11,6 @@ try{
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 }catch(Exception $e){
-	//$dbconnecterror = TRUE;
-
 	//This means we had some database issues
 	http_response_code(504);
 	exit();
@@ -22,35 +19,33 @@ try{
 
 //GET Task
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
-	if (!$dbconnecterror) {
-		try {
-			$sql = "SELECT * FROM doList";
-			$stmt = $dbh->prepare($sql);
-			$stmt->execute();
-			$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-			
-			// listID => listID
-			// listName -> taskName
-			// finishDate -> taskDate
-			// complete -> completed
-			
-			$new_data = [];
-			foreach($data as $task) {
-				$task['taskName'] = $task['listName'];
-				$task['taskDate'] = $task['finishDate'];
-				$task['completed'] = $task['complete'] ? true : false;
-				$new_data[] = $task;
-			}
-			
-			http_response_code(200);
-			echo json_encode($new_data);
-			exit();
-
-		} catch (PDOException $e) {
-			//This means we had some database issues
-			http_response_code(504);
-			echo 'could not get tasks';
-			exit();
+	try {
+		$sql = "SELECT * FROM doList";
+		$stmt = $dbh->prepare($sql);
+		$stmt->execute();
+		$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		
+		// listID => listID
+		// listName -> taskName
+		// finishDate -> taskDate
+		// complete -> completed
+		
+		$new_data = [];
+		foreach($data as $task) {
+			$task['taskName'] = $task['listName'];
+			$task['taskDate'] = $task['finishDate'];
+			$task['completed'] = $task['complete'] ? true : false;
+			$new_data[] = $task;
 		}
+		
+		http_response_code(200);
+		echo json_encode($new_data);
+		exit();
+
+	} catch (PDOException $e) {
+		//This means we had some database issues
+		http_response_code(504);
+		echo 'could not get tasks';
+		exit();
 	}
 }
